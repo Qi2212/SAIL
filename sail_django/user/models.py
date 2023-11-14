a08b56e62fc7549f4f6421d8813ad4191a122255
Qi2212 <2758486401@qq.com>
2023-11-14 20:57:02 +0800

    这是第一次删除

--- sail_django/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class UserProfile(models.Model):
-    username=models.CharField(max_length=30,verbose_name='用户名',unique=True,primary_key=True)
-    password=models.CharField(max_length=32,verbose_name='密码')
-#重置数据库表名
-    class Meta:
-        db_table = 'user_user_profile'
